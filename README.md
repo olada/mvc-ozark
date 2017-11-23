b005ff8496ef0286bb3b1151b4f267d1e023698c
dolah <david.olah@cofinpro.de>
2017-11-23 22:21:29 +0100

    Run presentation via npm and gulp

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # mvc-ozark
 
 ## Project Checkout
-
+Following commands need to be run once.
 ```
 git clone https://github.com/olada/mvc-ozark.git
 git submodule init
 git submodule update
+npm --prefix ./presentation install ./presentation
 ```
 ## Run this project
 aggregator-Projekt: mvn clean install
---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+ * Author: David Olah, Cofinpro AG
+ * Date: 23.11.2017, 21:48
+ */
+var gulp = require("gulp");
+var run = require("gulp-run");
+var rename = require("gulp-rename");
+var clean = require("gulp-clean");
+
+gulp.task("delete-default-presentation-index-file", function() {
+    console.log("Deleting ./presentation/index.html file");
+    return gulp.src("./presentation/index.html")
+            .pipe(clean({force: true}))
+});
+
+gulp.task("copy-presentation-html", ["delete-default-presentation-index-file"], function() {
+    console.log("Copying presentation.html from project root folder / to /presentation/index.html");
+    return gulp.src("presentation.html")
+        .pipe(gulp.dest("./presentation/"));
+});
+
+gulp.task("rename-presentation-to-index-in-presentation-folder", ["copy-presentation-html"], function() {
+    console.log("Renaming ./presentation/presentation.html to index.html");
+    return gulp.src("./presentation/presentation.html")
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest("./presentation"));
+});
+
+gulp.task("install-presentation-dependencies", function() {
+    return run("npm --prefix ./presentation install ./presentation").exec();
+});
+
+gulp.task("present", ["rename-presentation-to-index-in-presentation-folder", "install-presentation-dependencies"], function() {
+    console.log("Starting local server to serve presentation on localhost:8000");
+    return run("npm --prefix ./presentation run start").exec();
+});
