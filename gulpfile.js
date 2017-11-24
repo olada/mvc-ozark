9d153b8ea8c82b5760ab40c5e9874d9d065ef68e
dolah <david.olah@cofinpro.de>
2017-11-24 20:24:04 +0100

    umstrukturiert, paar zusätzliche Folien

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ var clean = require("gulp-clean");
 /**
  * Pipeline:
  * - Delete ./presentation/index.html (so we can easily replace it)
- * - Copy ./presentation.html to ./presentation/presentation.html
- * - Rename ./presentation/presentation.html in ./presentation/index.html
+ * - Copy ./slides/slides.html to ./presentation/slides.html
+ * - Rename ./presentation/slides.html in ./presentation/index.html
  * - Call npm start within ./presentation (spawns webserver displaying the presentation)
  */
 
@@ -21,24 +21,36 @@ gulp.task("delete-default-presentation-index-file", function() {
             .pipe(clean({force: true}))
 });
 
-gulp.task("copy-presentation-html", ["delete-default-presentation-index-file"], function() {
-    console.log("Copying presentation.html from project root folder / to /presentation/index.html");
-    return gulp.src("presentation.html")
+gulp.task("copy-slides.html", ["delete-default-presentation-index-file"], function() {
+    console.log("Copying slides.html from ./slides/ to ./presentation/");
+    return gulp.src("./slides/slides.html")
         .pipe(gulp.dest("./presentation/"));
 });
 
-gulp.task("rename-presentation-to-index-in-presentation-folder", ["copy-presentation-html"], function() {
-    console.log("Renaming ./presentation/presentation.html to index.html");
-    return gulp.src("./presentation/presentation.html")
+gulp.task("clean-presentation-etc-folder", function() {
+    console.log("Cleaning ./presentation/etc/");
+    return gulp.src("./presentation/etc/")
+        .pipe(clean());
+});
+
+gulp.task("copy-images-to-presentation-etc", ["clean-presentation-etc-folder"], function() {
+    console.log("Copying images folder from ./slides/ to ./presentation/etc/");
+    return gulp.src(["./slides/**/*", "!./slides/*"], {"base": "slides"})
+        .pipe(gulp.dest("./presentation/etc/"))
+});
+
+gulp.task("rename-slides-to-index-in-presentation-folder", ["copy-slides.html"], function() {
+    console.log("Renaming ./presentation/slides.html to index.html");
+    return gulp.src("./presentation/slides.html")
         .pipe(rename("index.html"))
         .pipe(gulp.dest("./presentation"));
 });
 
-gulp.task("present", ["rename-presentation-to-index-in-presentation-folder"], function() {
+gulp.task("present", ["rename-slides-to-index-in-presentation-folder", "copy-images-to-presentation-etc"], function() {
     console.log("Starting local server to serve presentation on localhost:8000");
     return run("npm --prefix ./presentation run start").exec();
 });
 
-// If changes are made to presentation.html, we will make sure that our pipeline is triggered again
+// If changes are made to any file within the ./slides/ directory, we will make sure that our pipeline is triggered again
 // That way, we only have to refresh the already running browser tab to retrieve the new presentation
-var watcher = gulp.watch("./presentation.html", ["rename-presentation-to-index-in-presentation-folder"]);
+var watcher = gulp.watch("./slides/**", ["rename-slides-to-index-in-presentation-folder", "copy-images-to-presentation-etc"]);
